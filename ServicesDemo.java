18af6f46789ca1114f1ad8ec8db718667af2aa68
jashachik <jashachik4@gmail.com>
2013-02-04 19:11:22 -0800

    Create ServicesDemo.java

---
+++ ServicesDemo.java
@@ -0,0 +1,38 @@
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class ServicesDemo extends Activity implements OnClickListener {
+    private static final String TAG = "ServicesDemo";
+	  Button buttonStart, buttonStop;
+	 
+	  @Override
+	  public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_services_demo);
+	 
+	    buttonStart = (Button) findViewById(R.id.buttonStart);
+	    buttonStop = (Button) findViewById(R.id.buttonStop);
+	 
+	    buttonStart.setOnClickListener(this);
+	    buttonStop.setOnClickListener(this);
+	  }
+	 
+	  public void onClick(View src) {
+	    switch (src.getId()) {
+	    case R.id.buttonStart:
+	      Log.d(TAG, "onClick: starting srvice");
+	      startService(new Intent(this, MyService.class));
+	      break;
+	    case R.id.buttonStop:
+	      Log.d(TAG, "onClick: stopping srvice");
+	      stopService(new Intent(this, MyService.class));
+	      break;
+	    }
+	  }
+	}
